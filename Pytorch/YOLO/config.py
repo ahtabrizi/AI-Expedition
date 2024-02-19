1fb3d8b0d00f852e7e155d39255478a953870368
redhood <a.tabrizi.mech@gmail.com>
2024-02-18 19:19:54 -0800

    add yolo config

---
+++ Pytorch/YOLO/config.py
@@ -0,0 +1,15 @@
+import os
+
+DATA_PATH = "data"
+CLASSES_PATH = os.path.join(DATA_PATH, "classes.json")
+
+BATCH_SIZE = 64
+EPOCHS = 135
+WARMUP_EPOCHS = 0
+LEARNING_RATE = 1e-4
+
+IMAGE_SIZE = (448, 448)
+
+S = 7  # Divide each image into a SxS grid
+B = 2  # Number of bounding boxes to predict
+C = 20  # Number of classes in the dataset
